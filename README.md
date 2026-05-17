c9bcbb34f807bb8afdf2e3316459bc63a58055b0
James-Marriner <jamesmarriner1@gmail.com>
2026-05-17 13:50:09 +0100

    Finished __init__ for lazy installation, moved license and updated README.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # [Re] Optimal Play of the Dice Game Pig
 ## STOR609 Coursework 3
 
-This project aims to replicate the results of `Optimal Play of the Dice Game Pig' by Tod Neller and Clifton Presser [1](#ref1). The scope of this project involves reproducing the optimal polciy and plots thereof which appear in the original paper for the games Piglet and Pig.
+This project aims to replicate the results of `Optimal Play of the Dice Game Pig' by Tod Neller and Clifton Presser [1](#ref1). The scope of this project involves reproducing the optimal policy and plots thereof which appear in the original paper for the games Piglet and Pig.
 
 ## Using This Repository
 
@@ -21,30 +21,31 @@ Install package & dependencies
 ```
 python -m pip install -r requirements.txt
 ```
-Run unit tests (change depending on directory structure)
+Run unit tests to confirm everything is working as intended
 ```
 python -m pytest
 ```
-
-When finished, the virtual environment should be deactivated
+Now feel free to explore and edit the repository. When finished, the virtual environment should be deactivated
 ```
 deactivate
 ```
 
 ### Installing the Package
 
-To exclusively install the package for use
+To simply use the package without the ability to edit or explore its functionality, install it for use in python by
 ```
 python -m pip install 'https://github.com/C-Rigby/Game_of_Pig_Assessment_3.git'
 ```
 
 ## Repository Structure
 
-The `examples/` folder contains notebooks illustrating how to use the code and play both Pig and Piglet.
+The `examples/` folder contains notebooks illustrating how to use the code and play both Pig and Piglet. These should be followed as a guide for anyone attempting to use the package themselves.
 
-The `experiments/` folder contains files used to replicate each of the results featured in the original paper.
+The `experiments/` folder contains files used to replicate each of the results featured in the original paper. Due to the nature of the replication objectives, there is some overlap here with the examples.
 
-The `src/` folder contains the package, license and a readme which can be used by future readers to explore the optimal policy under various parameterisations.
+The `src/` folder contains the package and license. which can be used by future readers to explore the optimal policy under various parameterisations.
+
+The `test/` folder contains the unit tests to verify the package is functioning as intended.
 
 The `article/` folder contains the files needed to create our report alongside the original article.
 
@@ -52,11 +53,10 @@ The `article/` folder contains the files needed to create our report alongside t
 
 ## Computation Times
 
-Computation time will vary depending on computer performance. All results were generated using x,y,z and the following times are indicative of relative workload.
+Computation time will vary depending on computer performance however run times found in the example notebooks are indicative of relative workload. All files were run under the following specifications.
 
 Operating System: Ubuntu 22.04.1
 CPU: Intel(R) Xeon(R) Gold 6248R CPU @ 3.00GHz x4
-Graphics Card: Not sure
 RAM: 16Gb
 
 
@@ -64,7 +64,7 @@ RAM: 16Gb
 Rick Cheng, James Marriner, Charlotte Rigby, Maria-Louiza Van den Bergh
 
 ## Version
-This package was created using Python ZZZ. Package versions used are
+This package was created using Python 3.10.8. Dependency requirements can be found in the file `requirements.txt`.
 
 ## References
 [1] <a id="ref1"></a>
--- src/optimal_pig/__init__.py
+++ src/optimal_pig/__init__.py
@@ -1,23 +1,83 @@
-from .pig import make_hold_at_policy, play_pig, make_spec
-from .piglet import make_hold_at_policy_piglet, make_always_flip_policy_piglet,play_piglet,piglet_make_spec
-from .value_iteration_fun import count_states,value_iteration,iter_states,optimal_policy_function,partitioned_value_iteration, valid_state_mask
-from .analysis_helpers import piglet_goal2_trace_states,piglet_goal2_exact_values,check_piglet_goal2_solution,figure2_data_from_result,plot_figure2_piglet_trace,
-          figure3_boundary_data,plot_figure3_policy_boundary,
-          figure4_cross_section_data,plot_figure4_cross_section,
-          figure5_reachable_data,plot_figure5_reachable_states,
-          figure6_reachable_continue_data,plot_figure6_reachable_continue_states,
-          figure7_probability_contour_data,plot_figure7_probability_contours,
-          summarize_solution,check_pig_start_probability,
+from .pig import (
+    make_hold_at_policy,
+    play_pig,
+    make_spec,
+)
 
-__version__= '0.1.0'
+from .piglet import (
+    make_hold_at_policy_piglet,
+    make_always_flip_policy_piglet,
+    play_piglet,
+    piglet_make_spec,
+)
 
-__all__ = ["count_states","value_iteration","iter_states","optimal_policy_function","partitioned_value_iteration","valid_state_mask",
-          "piglet_goal2_trace_states","piglet_goal2_exact_values","check_piglet_goal2_solution","figure2_data_from_result","plot_figure2_piglet_trace",
-          "figure3_boundary_data","plot_figure3_policy_boundary",
-          "figure4_cross_section_data","plot_figure4_cross_section",
-          "figure5_reachable_data","plot_figure5_reachable_states",
-          "figure6_reachable_continue_data","plot_figure6_reachable_continue_states",
-          "figure7_probability_contour_data","plot_figure7_probability_contours",
-          "summarize_solution","check_pig_start_probability",
-          "make_hold_at_policy_piglet","make_always_flip_policy_piglet","play_piglet","piglet_make_spec",
-          "make_hold_at_policy","play_pig","make_spec"]
+from .value_iteration_fun import (
+    count_states,
+    value_iteration,
+    iter_states,
+    optimal_policy_function,
+    partitioned_value_iteration,
+    valid_state_mask,
+)
+
+from .analysis_helpers import (
+    piglet_goal2_trace_states,
+    piglet_goal2_exact_values,
+    check_piglet_goal2_solution,
+    figure2_data_from_result,
+    plot_figure2_piglet_trace,
+    figure3_boundary_data,
+    plot_figure3_policy_boundary,
+    figure4_cross_section_data,
+    plot_figure4_cross_section,
+    figure5_reachable_data,
+    plot_figure5_reachable_states,
+    figure6_reachable_continue_data,
+    plot_figure6_reachable_continue_states,
+    figure7_probability_contour_data,
+    plot_figure7_probability_contours,
+    summarize_solution,
+    check_pig_start_probability,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # pig
+    "make_hold_at_policy",
+    "play_pig",
+    "make_spec",
+
+    # piglet
+    "make_hold_at_policy_piglet",
+    "make_always_flip_policy_piglet",
+    "play_piglet",
+    "piglet_make_spec",
+
+    # value iteration
+    "count_states",
+    "value_iteration",
+    "iter_states",
+    "optimal_policy_function",
+    "partitioned_value_iteration",
+    "valid_state_mask",
+
+    # analysis
+    "piglet_goal2_trace_states",
+    "piglet_goal2_exact_values",
+    "check_piglet_goal2_solution",
+    "figure2_data_from_result",
+    "plot_figure2_piglet_trace",
+    "figure3_boundary_data",
+    "plot_figure3_policy_boundary",
+    "figure4_cross_section_data",
+    "plot_figure4_cross_section",
+    "figure5_reachable_data",
+    "plot_figure5_reachable_states",
+    "figure6_reachable_continue_data",
+    "plot_figure6_reachable_continue_states",
+    "figure7_probability_contour_data",
+    "plot_figure7_probability_contours",
+    "summarize_solution",
+    "check_pig_start_probability",
+]
